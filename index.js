443843edb6fb57444b90dfbe359aca08c1d909f1
6kbgdb7k85-stack <6kbgdb7k86@privaterelay.appleid.com>
2026-06-18 14:13:53 -0400

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+function displayPosts(posts) {
+  const postList = document.querySelector("#post-list");
+  posts.forEach((post) => {
+    const postItem = document.createElement("li");
+    const postHeader = document.createElement("h1");
+    const postBody = document.createElement("p");
+    postHeader.textContent = post.title;
+    postBody.textContent = post.body;
+    postItem.append(postHeader, postBody);
+    postList.appendChild(postItem);
+  });
+}
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => response.json())
+  .then((posts) => displayPosts(posts))
+  .catch((error) => console.error(error));
